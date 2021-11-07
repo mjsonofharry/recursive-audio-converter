a8d7ba858798b45c2be48951f8e590470d4c79a6
MJ <mjsonofharry@gmail.com>
2021-11-07 10:03:45 -0500

    use a couple of classes

--- converter.py
+++ converter.py
@@ -1,43 +1,74 @@
 from argparse import ArgumentParser
+from dataclasses import dataclass
 import os
 import subprocess
+from typing import List
 
 
-def test_ffmpeg(ffmpeg_binary_path: str):
-    subprocess.call(
-        [ffmpeg_binary_path, "--help"],
-        stdout=open(os.devnull, "w"),
-        stderr=subprocess.STDOUT,
-    )
+class Ffmpeg:
+    def __init__(self, binary_path: str) -> None:
+        self.binary_path: str = binary_path
 
-
-def run_ffmpeg(ffmpeg_binary_path: str, input_path: str, output_path: str):
-    if os.path.exists(output_path):
-        raise RuntimeError(
-            f"Conversion would overwrite file at the following location: '{output_path}'"
+    def __exec(self, *args) -> None:
+        subprocess.call(
+            [self.binary_path, *args],
+            stdout=open(os.devnull, "w"),
+            stderr=subprocess.STDOUT,
         )
-    subprocess.call(
-        [ffmpeg_binary_path, "-i", input_path, output_path],
-        stdout=open(os.devnull, "w"),
-        stderr=subprocess.STDOUT,
-    )
+
+    def test(self) -> None:
+        self.__exec("--help")
+
+    def convert(self, input_path: str, output_path: str) -> None:
+        self.__exec("-i", input_path, output_path)
 
 
-def compute_output_path(
-    input_file_path: str,
-    root_input_path: str,
-    root_output_path: str,
-    output_extension: str,
-):
-    relative_file_path = os.path.relpath(path=input_file_path, start=root_input_path)
-    output_file_path = os.path.join(root_output_path, relative_file_path)
-    return (
-        input_file_path,
-        f"{os.path.splitext(output_file_path)[0]}.{output_extension}",
-    )
+@dataclass(frozen=True)
+class Converter:
+    root_input_path: str
+    root_output_path: str
+    output_extension: str
+    dry_run: bool
+    ffmpeg: Ffmpeg
+
+    def compute_output_path(self, input_file_path: str) -> str:
+        relative_file_path = os.path.relpath(
+            path=input_file_path, start=self.root_input_path
+        )
+        output_file_path = os.path.join(self.root_output_path, relative_file_path)
+        return f"{os.path.splitext(output_file_path)[0]}.{self.output_extension}"
+
+    def process_file(self, directory_path: str, file_name: str):
+        input_file_path: str = os.path.join(directory_path, file_name)
+        output_file_path: str = self.compute_output_path(
+            input_file_path=input_file_path
+        )
+        would_overwrite = os.path.exists(output_file_path)
+        print(f" - Input:  {input_file_path}")
+        print(f" {' !' if would_overwrite else ' '} Output: {output_file_path}")
+        if not self.dry_run:
+            os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+            if would_overwrite:
+                raise RuntimeError(f"Refusing to overwrite: '{output_file_path}'")
+            self.ffmpeg.convert(
+                input_path=input_file_path, output_path=output_file_path
+            )
+
+    def process_folder(self, directory_path: str, file_names: List[str]):
+        print(f"Current folder: {directory_path}")
+        for name in file_names:
+            self.process_file(directory_path=directory_path, file_name=name)
+
+    def run(self):
+        self.ffmpeg.test()
+        if not self.dry_run:
+            os.makedirs(name=self.root_output_path, exist_ok=True)
+        for step in os.walk(self.root_input_path):
+            directory_path, subdirectory_names, file_names = step
+            self.process_folder(directory_path=directory_path, file_names=file_names)
 
 
-def main():
+if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
         "-i", "--input", required=True, help="File system path to the input directory"
@@ -66,44 +97,11 @@ def main():
     )
     args = parser.parse_args()
 
-    root_input_path: str = os.path.abspath(args.input)
-    root_output_path: str = os.path.abspath(args.output)
-    output_format: str = args.format
-    ffmpeg_binary_path: str = args.binary
-    dry_run: bool = args.dry_run
-
-    test_ffmpeg(ffmpeg_binary_path=ffmpeg_binary_path)
-
-    if not dry_run:
-        os.makedirs(name=root_output_path, exist_ok=True)
-
-    for step in os.walk(root_input_path):
-        directory_path, subdirectory_names, file_names = step
-        if len(file_names) == 0:
-            continue
-        input_file_paths = [os.path.join(directory_path, name) for name in file_names]
-        output_file_paths = [
-            compute_output_path(
-                input_file_path=file_path,
-                root_input_path=root_input_path,
-                root_output_path=root_output_path,
-                output_extension=output_format,
-            )
-            for file_path in input_file_paths
-        ]
-        print(f"Current folder: {directory_path}")
-        for input_path, output_path in output_file_paths:
-            print(f" - Input:  '{input_path}'")
-            print(f"   Output: '{output_path}'")
-            print()
-            if not dry_run:
-                os.makedirs(os.path.dirname(output_path), exist_ok=True)
-                run_ffmpeg(
-                    ffmpeg_binary_path=ffmpeg_binary_path,
-                    input_path=input_path,
-                    output_path=output_path,
-                )
-
-
-if __name__ == "__main__":
-    main()
+    converter = Converter(
+        root_input_path=os.path.abspath(args.input),
+        root_output_path=os.path.abspath(args.output),
+        output_format=args.format,
+        dry_run=args.dry_run,
+        fmpeg=Ffmpeg(binary_path=args.binary),
+    )
+    converter.run()
